f7e29deb69538edfb1e8387c3e1794654cc93ab9
Asfaqur Rahman <abirftw@gmail.com>
2019-08-24 13:49:21 +0600

    UI of the main screen

---
+++ app/src/main/java/com/example/rockpaperscissors/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.rockpaperscissors;
+
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.main_activity);
+    }
+}
